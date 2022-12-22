2237e26aba00adc765cac4295719aef4bd211ef8
Alek30k <alemank23k@gmail.com>
2022-12-22 15:32:48 -0300

    add connect db

---
+++ controllers/auth.js
@@ -0,0 +1,9 @@
+export const register = (req, res) => {
+  //CHECK USER IF EXISTS
+  //CREATE A NEW USER
+  //Hash the password
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
