1e87dda5a58fca73a0f8d061d028aeb50b94ba4d
Alek30k <alemank23k@gmail.com>
2022-12-22 22:20:27 -0300

    add bcrypt

--- connect.js
+++ connect.js
@@ -3,6 +3,6 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "ale123",
+  password: "123456",
   database: "social",
 });
--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,10 @@ import { login, register, logout } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.post("login", login);
+router.post("/login", login);
 
-router.post("register", register);
+router.post("/register", register);
 
-router.post("logout", logout);
+router.post("/logout", logout);
 
 export default router;
