4c5f1981d64b22186de1695904a6ed7c9a0c5645
Alek30k <alemank23k@gmail.com>
2022-12-24 14:34:36 -0300

    last commit

--- index.js
+++ index.js
@@ -15,12 +15,15 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
+
 app.use(express.json());
+
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+
 app.use(cookieParser());
 
 // const storage = multer.diskStorage({
