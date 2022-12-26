12b0ab288dbd8c704c44cf7fa7bcb802c6ea600a
Alek30k <alemank23k@gmail.com>
2022-12-26 17:59:27 -0300

    last commit

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
-// import relationshipRoutes from "./routes/relationships.js";
+import relationshipRoutes from "./routes/relationships.js";
 import cors from "cors";
-// import multer from "multer";
+import multer from "multer";
 import cookieParser from "cookie-parser";
 
 //middlewares
@@ -26,28 +26,28 @@ app.use(
 
 app.use(cookieParser());
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "../client/public/upload");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + file.originalname);
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-// const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
-// app.post("/api/upload", upload.single("file"), (req, res) => {
-//   const file = req.file;
-//   res.status(200).json(file.filename);
-// });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
-// app.use("/api/relationships", relationshipRoutes);
+app.use("/api/relationships", relationshipRoutes);
 
 app.listen(8800, () => {
   console.log("API working!");
