eab79801158fc26b112e9123fbcf5c4b70e13666
Alek30k <alemank23k@gmail.com>
2022-12-30 11:44:37 -0300

    last commit

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
 app.use("/api/relationships", relationshipRoutes);
 
-app.listen(PORT, () => {
+app.listen(8800, () => {
   console.log("API working!");
 });
