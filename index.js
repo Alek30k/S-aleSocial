585c416ba8c0579075a038da5834c422756c6850
Alek30k <alemank23k@gmail.com>
2022-12-30 10:30:31 -0300

    last commit

--- index.js
+++ index.js
@@ -9,15 +9,13 @@ import relationshipRoutes from "./routes/relationships.js";
 import cors from "cors";
 import multer from "multer";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
 
 //middlewares
-dotenv.config();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 app.use(express.json());
 app.use(cors());
