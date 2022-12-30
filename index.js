c40d0edc7e15b736cb940c6ce79fa2e38b5297aa
Alek30k <alemank23k@gmail.com>
2022-12-30 10:39:43 -0300

    last commit

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json(file.filename);
 });
 
-const PORT = process.env.PORT || 8800;
+const PORT = process.env.POT || 8800;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
