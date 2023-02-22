d48c93f9dbf91e80425d7f7570fa18edc673b30c
Alek30k <alemank23k@gmail.com>
2023-02-22 16:46:11 -0300

    last commit

--- connect.js
+++ connect.js
@@ -1,7 +1,7 @@
 import mysql from "mysql";
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js";
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
