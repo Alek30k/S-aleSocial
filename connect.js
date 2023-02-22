5f13af098703f2789a808a5cc80c7caf896be898
Alek30k <alemank23k@gmail.com>
2023-02-22 15:43:47 -0300

    last commit

--- connect.js
+++ connect.js
@@ -1,19 +1,19 @@
-import mysql from "mysql";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js";
-
-export const db = mysql.createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  port: DB_PORT,
-});
-
 // import mysql from "mysql";
+// import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js";
 
 // export const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "123456",
-//   database: "social",
+//   host: DB_HOST,
+//   user: DB_USER,
+//   password: DB_PASSWORD,
+//   database: DB_NAME,
+//   port: DB_PORT,
 // });
+
+import mysql from "mysql";
+
+export const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "social",
+});
